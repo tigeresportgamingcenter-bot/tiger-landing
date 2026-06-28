31ff628b2bea6b59fd1eca99bb26d818ecba27ec
Tiger Esports <tigeresportgamingcenter@gmail.com>
2026-06-28 17:12:41 +0700

    Update official Facebook page

--- data/socialLinks.ts
+++ data/socialLinks.ts
@@ -1,7 +1,7 @@
 import type { SocialLinks } from "@/types";
 
 export const socialLinks: SocialLinks = {
-  facebook: "https://www.facebook.com/TigerEsportsGaming/",
+  facebook: "https://www.facebook.com/TigerEsportsGamingCenter/",
   zalo: "https://zalo.me/0834650555",
   hotline: "0834650555",
   googleMaps: null,
